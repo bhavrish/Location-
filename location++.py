43d8512e0124672ba78c5bcda5eff5477bb87db9
invaliduseo <alisonlee023@gmail.com>
2016-06-04 20:11:26 -0400

    Update location++.py

--- location++.py
+++ location++.py
@@ -1,5 +1,34 @@
+a = open( "DOITT_THEATER_01_13SEPT2010.csv" , "r" )
+aRL = a.readlines()
+a.close()
+
+b = open( "zip_codes.csv" , "r" )
+bRL = b.readlines()
+b.close()
+
+def listify( L ) :
+    for i in L :
+        p = L.index( i )
+        L[ p ] = L[ p ].strip()
+        L[ p ] = L[ p ].split( "," )
+    return L
+
+aL = listify( aRL )
+bL = listify( bRL )
+
 def loc2zip( w ) :
-  # n[ 1 ] is the column of towns in the zip_codes.csv file. n[ 0 ] is the column of zip codes
     for n in bL :
         if n[ 1 ] == w :
             return n[ 0 ]
+
+"""
+Theater: zip codes-col7, names-col1, addresses-col4
+"""
+
+def getN( loc , zipCode , name , address ) :
+    zipcode = loc2zip( loc )
+    for n in aL :
+        if n[ zipCode ] == zipcode :
+            print "The " + n[ name ] + " is located at " + n[ address ] + "."
+
+getN( "Lower East Side" , 7 , 1 , 4 )
