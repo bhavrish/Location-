bd0b315983c35894e3463e411ea7473549b12b43
invaliduseo <alisonlee023@gmail.com>
2016-06-04 20:08:03 -0400

    Update location++.py

--- location++.py
+++ location++.py
@@ -1,2 +1,5 @@
-hello
-hey
+def loc2zip( w ) :
+  # n[ 1 ] is the column of towns in the zip_codes.csv file. n[ 0 ] is the column of zip codes
+    for n in bL :
+        if n[ 1 ] == w :
+            return n[ 0 ]
